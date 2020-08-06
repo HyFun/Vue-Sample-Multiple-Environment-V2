e54f71ff12bfcdf392c1d8cd607347881319188e
heyongfeng <heyongfeng@cloudwalk.com>
2020-08-06 16:12:46 +0800

    初始化

---
+++ config/dev_01.env.js
@@ -0,0 +1,5 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"dev"',
+  BASE_SERVER_URL: '"这是一个dev_01环境的地址"'
+}
---
+++ config/prod-test.env.js
@@ -0,0 +1,5 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"pro"',
+  BASE_SERVER_URL: '"这是一个prod-test环境的地址"'
+}
